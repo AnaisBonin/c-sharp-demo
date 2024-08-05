0d77b49e28da1bf49ec3557d8503f99bc1b7c8da
AnaisBonin <anaisboninfr@gmail.com>
2024-08-05 15:59:05 +0200

    Create a UserInteraction class with WelcomeUser, Welcome name methods

---
+++ CSharp/CSharpMac/Views/UserInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharpMac
+{
+	public class UserInteraction
+	{
+		public string User { get; set; }
+
+		public UserInteraction()
+		{
+			this.User = "no one is here!";
+		}
+
+		public void WelcomeUser()
+		{
+            Console.WriteLine("Bienvenue dans notre application console!");
+        }
+
+		public void GetUserName()
+		{
+            Console.Write("Veuillez entrer votre nom: ");
+            string userName = Console.ReadLine();
+
+			if (userName != null)
+			{
+				this.User = userName;
+			}
+        }
+
+		public void WelcomeName()
+		{
+            Console.WriteLine($"Bonjour, {this.User}!");
+        }
+	}
+}
+

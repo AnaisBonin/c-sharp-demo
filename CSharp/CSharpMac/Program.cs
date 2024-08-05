59c28d46c89ea33f15bffacb73ae4c4915a21174
AnaisBonin <anaisboninfr@gmail.com>
2024-08-05 14:49:25 +0200

    Launch basic program with name and age

--- CSharp/CSharpMac/Program.cs
+++ CSharp/CSharpMac/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Afficher un message de bienvenue
+            Console.WriteLine("Bienvenue dans notre application console!");
+
+            // Lire le nom de l'utilisateur
+            Console.Write("Veuillez entrer votre nom: ");
+            string userName = Console.ReadLine();
+
+            // Saluer l'utilisateur
+            Console.WriteLine($"Bonjour, {userName}!");
+
+            // Lire l'âge de l'utilisateur
+            Console.Write("Veuillez entrer votre âge: ");
+            int userAge;
+            while (!int.TryParse(Console.ReadLine(), out userAge))
+            {
+                Console.Write("Veuillez entrer un âge valide: ");
+            }
+
+            // Afficher un message selon l'âge
+            if (userAge < 18)
+            {
+                Console.WriteLine("Vous êtes mineur.");
+            }
+            else
+            {
+                Console.WriteLine("Vous êtes majeur.");
+            }
+
+            // Fin du programme
+            Console.WriteLine("Appuyez sur une touche pour terminer...");
+            Console.ReadKey();
+        }
+    }
+}

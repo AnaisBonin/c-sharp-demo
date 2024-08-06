e2500e87ebe26e1532f591a109203a31c47e363c
AnaisBonin <anaisboninfr@gmail.com>
2024-08-06 13:58:59 +0200

    Create UserService to define User status

---
+++ CSharp/CSharpMac/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CSharpMac
+{
+	public class UserService
+	{
+        // private / local variables must be named with _
+        readonly int _legalAge = 18;
+
+        public string DefineUserStatus(int userAge)
+		{
+            if (userAge < _legalAge)
+            {
+                return "mineur";
+            }
+            else
+            {
+                return "majeur";
+            }
+        }
+
+	}
+}

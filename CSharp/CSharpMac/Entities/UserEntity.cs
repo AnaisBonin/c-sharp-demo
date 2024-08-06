1b77d4981d1c3781f8d8f7153c778005ef5d2701
AnaisBonin <anaisboninfr@gmail.com>
2024-08-06 15:52:29 +0200

    Add time exampes

---
+++ CSharp/CSharpMac/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CSharpMac
+{
+	public class UserEntity
+	{
+        public string? UserName { get; set; }
+        public int? UserAge { get; set; }
+	}
+}
+
---
+++ CSharp/TimeManagement/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TimeManagement;
+
+namespace CSharpMac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("/////////////");
+            TimeService.PrintFromDate();
+            Console.WriteLine("/////////////");
+
+            Console.WriteLine("************");
+            TimeService.PrintFromToday();
+            Console.WriteLine("************/");
+
+            Console.WriteLine("Appuyez sur une touche pour terminer...");
+            Console.ReadKey();
+        }
+    }
+}
